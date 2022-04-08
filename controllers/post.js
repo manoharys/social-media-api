2ea7644b1a360d4ef4764403389efc8fc90feff7
manoharys <manumanohar1527@gmail.com>
2022-04-09 01:40:26 +0530

    feat: added likes controller

--- controllers/post.js
+++ controllers/post.js
@@ -1,5 +1,6 @@
 const Post = require("../models/Post");
 const User = require("../models/User");
+const ObjectId = require("mongodb").ObjectId;
 
 // @route POST api/posts
 // @desc Create a post
@@ -25,6 +26,47 @@ exports.createPost = async (req, res) => {
 
     res.status(201).json({
       message: "Post created successfully",
+      success: true,
+      post,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+// @route GET api/posts
+// @desc likeAndUnlike a post
+// @access private
+
+exports.likeAndUnlike = async (req, res) => {
+  try {
+    const post = await Post.findOne({ _id: ObjectId(req.params.id) }).exec();
+
+    // check post exists
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post not found",
+      });
+    }
+
+    const userId = req.user._id.toString();
+    let type;
+
+    if (post.likes.includes(userId)) {
+      post.likes = post.likes.filter((id) => id.toString() !== userId);
+      type = "unlike";
+    } else {
+      post.likes.push(userId);
+      type = "like";
+    }
+    await post.save();
+
+    res.status(200).json({
+      message: `${type} post successfully`,
       success: true,
       post,
     });
--- routes/post.js
+++ routes/post.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const { createPost } = require("../controllers/post");
+const { createPost, likeAndUnlike } = require("../controllers/post");
 const router = express.Router();
 const { auth } = require("../middlewares/auth");
 
 router.route("/create").post(auth, createPost);
+
+router.route("/likepost/:id").post(auth, likeAndUnlike);
 
 module.exports = router;
